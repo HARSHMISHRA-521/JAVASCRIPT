75b080ac4ad3b0de303a6f1ab94fbef81c39474a
HARSH <mishraharshmishra2020@gmail.com>
2023-12-31 02:58:52 +0530

    javascript course

---
+++ course_codes/04_Practise_Set1.js
@@ -0,0 +1,32 @@
+//chapter 1 - q1
+{
+let a = "Harsh"
+let b =6
+console.log(a+b) //Harsh6
+}
+
+//chapter 1 - q2
+console.log(typeof (a + b)) //string
+
+//chapter 1 - q3
+const a ={
+    name: "Harsh",
+    age: 21,
+    isPrincipal : false,
+    isMarried: true
+} 
+a = "Harsh" // we cannot change it to hold  a string later
+
+
+//chapter 1 - q4
+a["friend"] = "Karuna"
+
+//chapter 1 - q5
+const dictionary = {
+    aapreciate : "recognize the full worth of",
+    kind : "being humble",
+    happy : "being happy",
+    sad : "being sad",
+    proud : "being proud"
+
+}
